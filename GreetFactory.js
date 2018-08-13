95c7547b8c699d4fb0d13b982410a07b9d5fd8ec
vusi <baloyi.trev@gmail.com>
2018-08-13 15:36:13 +0200

    removed commented-out code

--- GreetFactory.js
+++ GreetFactory.js
@@ -13,18 +13,7 @@ module.exports = function (greetData) {
         let firstCh = inputName.charAt(0).toUpperCase();
         inputName = firstCh + tmpString
 
-        //counter init
-        // if (localStorage['counter'] === undefined) {
-        //     localStorage.setItem('counter', JSON.stringify(0));
-        // }
 
-        // if (greetMap[inputName] === undefined) {
-        //     greetMap[inputName] = 0;
-        //     var counter = JSON.parse(localStorage.getItem('counter'));
-        //     //increament to the counter if the user has not been registered to the map
-        //     localStorage.setItem('counter', JSON.parse(counter + 1));
-
-        // }
         //return a greeting based on the given language
         if (language === 'English') {
             return 'Hello ' + inputName + '!'
@@ -44,20 +33,7 @@ module.exports = function (greetData) {
         };
     };
 
-    //function that returns the checked language button 
-    // let setLang = function (value) {
-    //     var lang = '';
-    //     if (value === 'English') {
-    //         lang = 'English';
-    //     }
-    //     if (value === 'Zulu') {
-    //         lang = 'Zulu';
-    //     }
-    //     if (value === 'Tsonga') {
-    //         lang = 'Tsonga';
-    //     }
-    //     return lang
-    // }
+
     //factory returns
 
     return {
