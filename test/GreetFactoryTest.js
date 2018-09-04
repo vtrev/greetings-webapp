c9db4b81c9a7e4996d9dc1200409b0dce5a06f21
vusi <baloyi.trev@gmail.com>
2018-09-04 10:35:44 +0200

    enabled ssl

--- test/GreetFactoryTest.js
+++ test/GreetFactoryTest.js
@@ -21,7 +21,8 @@ const pool = new Pool({
     host: 'ec2-54-225-92-1.compute-1.amazonaws.com',
     database: 'dvpi1u6n33sj8',
     password: '2e8ef2ec5aad80551c6997707d10ab7ca405410e7c8a9233d283614b0c059d18',
-    port: 5432
+    port: 5432,
+    ssl: true
 });
 
 const greet = greetings(pool);
